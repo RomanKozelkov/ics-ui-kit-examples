d77a4090b330d5ee8d2ec5a415a4e7f4897920f4
valeriya.demihova <valeriya.demihova@ics-it.ru>
2026-05-06 13:01:03 +0300

    fix

--- src/examples/SidebarNavigation/NavigationItemCounter.tsx
+++ src/examples/SidebarNavigation/NavigationItemCounter.tsx
@@ -4,7 +4,7 @@ interface NavigationItemCounterProps {
 
 export function NavigationItemCounter({ children }: NavigationItemCounterProps) {
 	return (
-		<div className="h-4 min-w-4 rounded-full rounded-bl-none bg-status-warning-hover px-1 py-[3px] pt-1 text-center text-[10px] leading-none text-secondary-bg">
+		<div className="min-w-4 rounded-full rounded-bl-none bg-status-warning-hover px-1 py-[3px] text-center text-[10px] leading-none text-secondary-bg">
 			{children}
 		</div>
 	);
