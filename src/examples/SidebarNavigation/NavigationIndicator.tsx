d16934f10c2de90d8a671676704469c3ea3b10ba
valeriya.demihova <valeriya.demihova@ics-it.ru>
2026-05-19 15:01:39 +0300

    f

--- src/examples/SidebarNavigation/NavigationIndicator.tsx
+++ src/examples/SidebarNavigation/NavigationIndicator.tsx
@@ -3,7 +3,7 @@ import { getIndicatorOffset } from "./getIndicatorOffset";
 export function NavigationIndicator({ level }: { level: number }) {
 	return (
 		<span
-			className="pointer-events-none absolute top-0 my-0.5 h-6 w-0.5 rounded-xs bg-primary-accent"
+			className="pointer-events-none absolute top-0 my-1.5 h-4 w-0.5 rounded-xs bg-primary-accent"
 			style={{ left: getIndicatorOffset(level) }}
 		/>
 	);
