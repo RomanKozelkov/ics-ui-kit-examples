d376b8e11d8fbf8ac5a798beb4c1069a416b5cf7
valeriya.demihova <valeriya.demihova@ics-it.ru>
2026-06-02 14:22:37 +0300

    fix

--- src/examples/SidebarNavigation/components/sidebar-navigation/sidebar-insertion-line/VerticalLineSegment.tsx
+++ src/examples/SidebarNavigation/components/sidebar-navigation/sidebar-insertion-line/VerticalLineSegment.tsx
@@ -1,5 +1,5 @@
 export function VerticalLineSegment() {
 	return (
-		<span className="pointer-events-none absolute inset-y-0 -top-0.5 bottom-0.5 left-0 w-px bg-primary-border" />
+		<span className="pointer-events-none absolute inset-y-0 -top-0.5 bottom-0.5 left-0 z-10 w-px bg-primary-border" />
 	);
 }
