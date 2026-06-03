62c2c4dc3ccadaabe457aba70400b0e9822fca77
valeriya.demihova <valeriya.demihova@ics-it.ru>
2026-06-03 10:39:05 +0300

    fix

--- src/examples/SidebarNavigation/components/sidebar-navigation/sidebar-drag-drop/NavigationDragPreview.tsx
+++ src/examples/SidebarNavigation/components/sidebar-navigation/sidebar-drag-drop/NavigationDragPreview.tsx
@@ -1,6 +1,6 @@
 export function NavigationDragPreview({ name }: { name: string }) {
 	return (
-		<div className="flex h-7 items-center rounded-md bg-secondary-bg p-1.5 pl-2 text-sm font-medium opacity-85 shadow-md">
+		<div className="flex h-7 items-center rounded-md bg-secondary-bg p-1.5 pl-2 text-sm font-normal text-primary-fg opacity-85 shadow-md">
 			{name}
 		</div>
 	);
