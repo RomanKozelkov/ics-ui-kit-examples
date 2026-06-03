4da5ddce0ee0323862f790db1bd875d08a066430
valeriya.demihova <valeriya.demihova@ics-it.ru>
2026-06-03 15:19:28 +0300

    f

--- src/examples/SidebarNavigation/components/sidebar-navigation/sidebar-insertion-line/VerticalLineSegment.tsx
+++ src/examples/SidebarNavigation/components/sidebar-navigation/sidebar-insertion-line/VerticalLineSegment.tsx
@@ -3,6 +3,7 @@ import { cn } from "ics-ui-kit/lib/utils";
 export function VerticalLineSegment({ className }: { className?: string }) {
 	return (
 		<span
+			data-sidebar="vertical-line-segment"
 			className={cn(
 				"pointer-events-none absolute -top-0.5 bottom-0 z-10 w-px rounded-full bg-primary-border",
 				className
