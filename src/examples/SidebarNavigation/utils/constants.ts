ad7fa15bc300eeebe1900c16d4b67b783c629704
valeriya.demihova <valeriya.demihova@ics-it.ru>
2026-06-09 17:54:42 +0300

    f

--- src/examples/SidebarNavigation/utils/constants.ts
+++ src/examples/SidebarNavigation/utils/constants.ts
@@ -2,5 +2,5 @@ export const SIDEBAR_MENU_SUB_INDENT = 24;
 export const INSERTION_BUTTON_SIZE = 12;
 
 export const DRAG_ACTIVATION_DISTANCE = 8;
-export const DROP_AFTER_ZONE_PX = 6;
+export const DROP_AFTER_ZONE_PX = 4;
 export const AUTO_EXPAND_DELAY_MS = 500;
